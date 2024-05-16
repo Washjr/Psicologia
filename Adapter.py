2274a94d6acc1e7fc86a6a9f24666cdb7d360d75
Washington Júnior <104223792+Washjr@users.noreply.github.com>
2024-05-16 10:28:55 -0300

    Update Adapter.py

--- Adapter.py
+++ Adapter.py
@@ -1,21 +1,41 @@
 public class Client {
-    
+    private Target target;
+
+    public Client(Target target) {
+        this.target = target;        
+    }
+
+    // Código restante...
 }
 
 public interface Target {
-    
+    public void operation(){
+        // Lógica de funcionamento...
+    }
 }
 
 public class Adapter implements Target extends Adaptee {
-    
-}
-
-public class Adaptee {
-    
+    public void operation(){
+        specific_operation();
+    }    
 }
 
 /*
 public class Adapter implements Target {
     private Adaptee adaptee;
+
+    public Adapter(Adaptee adaptee) {
+        this.adaptee = adaptee;
+    }
+
+    public void operation(){
+        adaptee.specific_operation();
+    }
 }
 */
+
+public class Adaptee {
+    public void specific_operation(){
+        // Lógica de funcionamento... 
+    }    
+}
