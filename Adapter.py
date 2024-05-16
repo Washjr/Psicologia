e5b24ae49edafa556a0ef2ebca0cb61c9b571d54
Washington Júnior <104223792+Washjr@users.noreply.github.com>
2024-05-16 10:06:22 -0300

    Update Adapter.py

--- Adapter.py
+++ Adapter.py
@@ -1,1 +1,21 @@
+public class Client {
+    
+}
 
+public interface Target {
+    
+}
+
+public class Adapter implements Target extends Adaptee {
+    
+}
+
+public class Adaptee {
+    
+}
+
+/*
+public class Adapter implements Target {
+    private Adaptee adaptee;
+}
+*/
